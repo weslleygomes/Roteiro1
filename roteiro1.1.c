e2b273ea9ef15116d486ac2350d6937cae8fd69d
weslley_45 <weslley_45@hotmail.com>
2017-08-20 14:26:35 -0300

    roteiro 1 - 1

---
+++ roteiro1.1.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <math.h>
+
+int main(){
+    int idade;
+    float preco;
+
+    printf("Digite a idade e o preco: ");
+    scanf("%d", &idade);
+    scanf("%f", &preco);
+
+    if(idade>18){
+        printf("Desconto: 20%%\n"
+               "Preco final: R$%.02f", preco-preco*0.2);
+    }else{
+        printf("Desconto: 10%%\n"
+               "Preco final: R$%.02f", preco-preco*0.1);
+    }
+    return 0;
+}
